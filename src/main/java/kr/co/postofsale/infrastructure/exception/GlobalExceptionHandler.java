128aad08e2d80f74dd05178ee0d46e24c5aa3c66
홍혁준 <jjj8337721@naver.com>
2022-06-03 01:25:38 +0900

    sales 수정3

--- src/main/java/kr/co/postofsale/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/postofsale/infrastructure/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package kr.co.postofsale.infrastructure.exception;
 
 import kr.co.postofsale.common.ResponseFormat;
+import lombok.extern.log4j.Log4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  *  즉, 전역에서 발생할 수 있는 예외를 잡아 처리해주는 annotation.
  */
 @RestControllerAdvice
+@Log4j
 public class GlobalExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     /**@ExceptionHandler
      * GlovalExceptionHandler로 넘어온 예외가 NotFoundException 일 때, 해당 메소드가 실행.
@@ -25,5 +31,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = BadRequestException.class)
     public ResponseFormat<Void> badRequestExcetpionHandler(BadRequestException badRequestException){
         return ResponseFormat.fail(badRequestException.getMessage());
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseFormat<Void> exceptionHandler(Exception exception) {
+        logger.info("메세지 !!!!");
+        return ResponseFormat.fail(exception.getMessage());
     }
 }
