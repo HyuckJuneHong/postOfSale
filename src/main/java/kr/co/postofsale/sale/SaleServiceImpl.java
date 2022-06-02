90222abdb151df0492650a4e720b75ec13b70917
홍혁준 <jjj8337721@naver.com>
2022-06-02 22:46:00 +0900

    sales 수정3

--- src/main/java/kr/co/postofsale/sale/SaleServiceImpl.java
+++ src/main/java/kr/co/postofsale/sale/SaleServiceImpl.java
@@ -8,7 +8,6 @@ import kr.co.postofsale.member.enumClass.MemberRole;
 import kr.co.postofsale.product.ProductEntity;
 import kr.co.postofsale.product.ProductRepository;
 import kr.co.postofsale.sale.enumClass.SalePayment;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class SaleServiceImpl implements SaleService{
 
     private final SaleRepository saleRepository;
